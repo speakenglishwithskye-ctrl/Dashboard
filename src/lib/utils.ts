46a3d3d5d7832e6ad5303040f7f183ba38fa5668
speakenglishwithskye-ctrl <speakenglishwithskye@gmail.com>
2026-05-12 19:05:10 +0630

    Change currency format from USD to MMK

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'MMK',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount)
